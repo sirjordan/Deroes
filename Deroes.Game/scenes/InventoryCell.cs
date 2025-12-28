6b22c79ae59494d49b25587a57a0384e9798571d
m_marinoff <martin@jobshop.bg>
2025-12-28 19:04:15 +0200

    Inventory UI changed.

---
+++ Deroes.Game/scenes/InventoryCell.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System;
+using System.Runtime.InteropServices.JavaScript;
+
+public partial class InventoryCell : Panel
+{
+	[Export] public Vector2 HoverScale = new Vector2(1.25f, 1.25f);
+	[Export] public float TweenTime = 0.15f;
+
+	[Export] public int ShadowSize = 6;
+	[Export] public Vector2 ShadowOffset = new(0, 0);
+	[Export] public Color ShadowColor = new(0, 0, 0, 0.35f);
+
+	private Tween _tween;
+	private Vector2 _originalScale;
+	private StyleBoxFlat _style;
+
+	public override void _Ready()
+	{
+		_originalScale = Scale;
+		PivotOffset = Size / 2;
+		Resized += () => PivotOffset = Size / 2;
+
+		// Clone the panel style
+		_style = (StyleBoxFlat)GetThemeStylebox("panel").Duplicate();
+		AddThemeStyleboxOverride("panel", _style);
+
+		DisableShadow();
+
+		MouseEntered += OnMouseEntered;
+		MouseExited += OnMouseExited;
+	}
+
+	private void OnMouseEntered()
+	{
+		EnableShadow();
+		StartTween(HoverScale);
+	}
+
+	private void OnMouseExited()
+	{
+		DisableShadow();
+		StartTween(_originalScale);
+	}
+
+	private void StartTween(Vector2 targetScale)
+	{
+		_tween?.Kill();
+		_tween = CreateTween();
+		_tween.SetEase(Tween.EaseType.Out);
+		_tween.SetTrans(Tween.TransitionType.Quad);
+		_tween.TweenProperty(this, "scale", targetScale, TweenTime);
+	}
+
+	private void EnableShadow()
+	{
+		_style.ShadowSize = ShadowSize;
+		_style.ShadowOffset = ShadowOffset;
+		_style.ShadowColor = ShadowColor;
+	}
+
+	private void DisableShadow()
+	{
+		_style.ShadowSize = 0;
+	}
+}
